e142c77385e99f66f4caf97a4fa92b79ee574a55
Julio Machado <jsmacbusiness@gmail.com>
2023-06-16 16:30:47 -0300

    Feat: configuring react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+// CSS
 import "./App.css";
+// packages
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// components
+// pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <main className="container">
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </main>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+// CSS
+import styles from "./Home.module.css";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello World - Mini Blog - Home</h1>
+    </div>
+  );
+};
+
+export default Home;
