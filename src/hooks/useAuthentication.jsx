550f25ee78beb3ec5dfcd415f6780bb0bd6071cb
Julio Machado <jsmacbusiness@gmail.com>
2023-06-17 18:26:40 -0300

    Feat: user login

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -74,6 +74,32 @@ export const useAuthentication = () => {
     signOut(auth);
   };
 
+  // Login - sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "A senha foi inserida incorretamente.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
@@ -84,5 +110,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };
