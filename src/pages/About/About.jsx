2b28683d167f4e9f759ef20b04d0024f3cb4841a
Julio Machado <jsmacbusiness@gmail.com>
2023-06-17 17:40:21 -0300

    Feat: ending the page about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,26 @@
 // CSS
 import styles from "./About.module.css";
+// packages
+import { Link } from "react-router-dom";
 import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>Hello World - Mini Blog - About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com <b>React</b> no Front-end e{" "}
+        <b>Firebase</b> no Back-end.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar post
+      </Link>
+      <div className={styles.images}>
+        <img src="/react.png" />
+        <img src="/firebase.png" />
+      </div>
     </div>
   );
 };
