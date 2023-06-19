dc9a4ca3a394477d6e0546c066cab71a745364c0
Julio Machado <jsmacbusiness@gmail.com>
2023-06-19 17:23:27 -0300

    Feat: displaying search results

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -4,16 +4,31 @@ import styles from "./Search.module.css";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
 // packages
+import { Link } from "react-router-dom";
 import React from "react";
+// components
+import PostDetail from "../../components/PostDetail/PostDetail";
 
 const Search = () => {
   const query = useQuery();
   const search = query.get("q");
 
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
-    <div>
+    <div className={styles.search_container}>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts relacionado a sua busca...</p>
+            <Link to="/" className="btn">
+              Voltar
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
