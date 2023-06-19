402c0cd89dc373a538d0a22eda0c1f77ece72987
Julio Machado <jsmacbusiness@gmail.com>
2023-06-19 16:37:08 -0300

    Feat: displaying posts on homepage

---
+++ src/components/PostDetail/PostDetail.jsx
@@ -0,0 +1,29 @@
+// CSS
+import styles from "./PostDetail.module.css";
+
+import { Link } from "react-router-dom";
+
+import React from "react";
+
+const PostDetail = ({ post }) => {
+  return (
+    <div>
+      <img src={post.image} alt={post.title} />
+      <h2>{post.title}</h2>
+      <p>{post.createdBy}</p>
+      <div>
+        {post.tags.map((tag) => (
+          <p key={tag}>
+            <span>#</span>
+            {tag}
+          </p>
+        ))}
+      </div>
+      <Link to={`/posts/${post.id}`} className="btn">
+        Ler
+      </Link>
+    </div>
+  );
+};
+
+export default PostDetail;
--- src/hooks/useFetchDocuments.jsx
+++ src/hooks/useFetchDocuments.jsx
@@ -1,60 +1,74 @@
 import { useState, useEffect } from "react";
-import { db } from "../firebase/Config";
+import { db } from "../firebase/config";
 import {
   collection,
   query,
   orderBy,
   onSnapshot,
   where,
-  QuerySnapshot,
 } from "firebase/firestore";
 
 export const useFetchDocuments = (docCollection, search = null, uid = null) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
-  const [load, setLoad] = useState(null);
+  const [loading, setLoading] = useState(null);
 
   // deal with memory leak
   const [cancelled, setCancelled] = useState(false);
 
   useEffect(() => {
     async function loadData() {
-      if (cancelled) return;
+      if (cancelled) {
+        return;
+      }
 
-      setLoad(true);
+      setLoading(true);
 
       const collectionRef = await collection(db, docCollection);
 
       try {
         let q;
 
-        // busca
-        // dashboard
+        if (search) {
+          q = await query(
+            collectionRef,
+            where("tags", "array-contains", search),
+            orderBy("createdAt", "desc")
+          );
+        } else if (uid) {
+          q = await query(
+            collectionRef,
+            where("uid", "==", uid),
+            orderBy("createdAt", "desc")
+          );
+        } else {
+          q = await query(collectionRef, orderBy("createdAt", "desc"));
+        }
 
-        q = await query(collectionRef, orderBy("createdAt", "desc"));
-
-        await onSnapshot(q, (QuerySnapshot) => {
+        await onSnapshot(q, (querySnapshot) => {
           setDocuments(
-            QuerySnapshot.docs.map((doc) => ({
+            querySnapshot.docs.map((doc) => ({
               id: doc.id,
               ...doc.data(),
             }))
           );
         });
-
-        setLoad(false);
       } catch (error) {
         console.log(error);
         setError(error.message);
-        setLoad(false);
       }
+
+      setLoading(false);
     }
+
     loadData();
   }, [docCollection, search, uid, cancelled]);
+
+  console.log(documents);
 
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
 
-  return documents, loading, error;
+  return { documents, loading, error };
 };
