c8ebea1653e67ef5be76359216b22550e8762345
Julio Machado <jsmacbusiness@gmail.com>
2023-06-19 18:08:32 -0300

    Feat: structuring the dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,35 @@
 // CSS
 import styles from "./Dashboard.module.css";
-// pachages
+// hooks
+import { Link } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+// packages
 import React from "react";
 
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  // posts do usuario
+  const posts = [];
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
   );
 };
