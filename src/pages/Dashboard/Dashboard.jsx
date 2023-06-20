7c7d60b06fce2b2322678ec4b346c12ab9a46882
Julio Machado <jsmacbusiness@gmail.com>
2023-06-20 13:10:59 -0300

    Feat: deleting post

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import styles from "./Dashboard.module.css";
 import { Link } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 // packages
 import React from "react";
 
@@ -14,7 +15,7 @@ const Dashboard = () => {
   // posts do usuario
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
-  const deleteDocument = (id) => {};
+  const { deleteDocument } = useDeleteDocument("posts");
 
   if (loading) {
     return <p>Carregando</p>;
